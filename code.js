7c6952b5bec7f7d43f9050e4586548f7393d5b33
Valentina Versari <versavale@gmail.com>
2018-10-06 20:29:06 +0200

    svg added

---
+++ code.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+
+var top1 = $('#hero').offset().top;
+var top2 = $('#about').offset().top;
+var top3 = $('#projects').offset().top;
+var top4 = $('#contact').offset().top;
+
+$(document).scroll(function() {
+  var scrollPos = $(document).scrollTop();
+  if (scrollPos >= top1 && scrollPos < top2-150) {
+    $('#navbar').css('background-color', 'transparent');
+  } else if (scrollPos >= top2-150 && scrollPos < top3) {
+    $('#navbar').css('background-color', '#FF0844');
+  } else if (scrollPos >= top3 && scrollPos < top4) {
+    $('#navbar').css('background-color', 'transparent');
+  } else if (scrollPos >= top4) {
+    $('#navbar').css('background-color', 'transparent');
+  }
+});
+
+});
